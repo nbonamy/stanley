e37a800e7271a0473f403f0c70dbfb24da7beece
Nicolas Bonamy <nicolas@bonamy.fr>
2020-04-25 17:55:07 -0500

    progress indicator

---
+++ lib/progress.dart
@@ -0,0 +1,27 @@
+
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Progress extends StatelessWidget {
+
+  final Color color;
+  const Progress({
+    Key key,
+    this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    if (Platform.isIOS) {
+      return CupertinoActivityIndicator();
+    } else {
+      return CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(color),
+      );
+    }
+  }
+
+}
